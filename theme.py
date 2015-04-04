451a1c00e41f8731a77f6fa2e5d1a962468311cb
su-kaiyao <1250471161@qq.com>
2015-04-04 15:54:42 +0800

    init

---
+++ theme.py
@@ -0,0 +1,14 @@
+name = 'catstrap'
+author = 'su-kaiyao'
+homepage = 'https://github.com/su-kaiyao/catsup-theme-catstrap'
+post_per_page = 10
+vars = {
+    "github": "su-kaiyao",
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "一个博客系统"
+        }
+    ]
+}
